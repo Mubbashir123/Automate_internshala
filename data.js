db87d16108c2f4ed0dab527cb25fde06a292966b
Mohd Mubbashir <www.mohdmubbashir@gmail.com>
2025-01-09 18:38:21 +0530

    First commit

---
+++ data.js
@@ -0,0 +1,17 @@
+const dataFile = [
+  {
+    "College": "Jamia Millia Islamia",  // Corrected college name
+    "Degree": "Bachelor of Technology (B.Tech)",  // Degree
+    "Stream": "Electrical Engineering",  // Stream of study
+    "Percentage": "91",  // Percentage in the course
+    "Training": "Data Structures and Algorithms, Web Development",  // Training/skills
+    "Organization": "XYZ Pvt Ltd.",  // Organization where training or work was done
+    "description": "XYZ is a great institution where I got to learn the not only the basics of data structures and algorithms but I also got familiar with Web Development and Competitive Programming. Here, the main focus is on concept building and it was a great start for me.",  // Description of your experience
+    "link": "https://github.com/xyz/Task1",  // Portfolio or GitHub link
+    "hiringReason": "Over the past few years, I have acquired relevant skills and experience, which I shall bring to your organization. I have also worked tirelessly on my communication abilities and teamwork skills, which I will put to use in my future career, which would be in your organization if I am selected for the position.",  // Reason for applying
+    "availability": "Yes, I will be available",  // Availability for the internship
+    "rating": "4",  // Your self-assessed rating of your skills (can be based on a scale of 1-5)
+  }
+];
+
+module.exports = dataFile;
